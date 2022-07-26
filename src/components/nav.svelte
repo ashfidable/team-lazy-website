46a2c93bbf9c8f88710614d10ac95eb1acb6e3c6
RoastedBrotato <waleedajaz1996@gmail.com>
2022-07-27 02:07:52 +0300

    initial commit

---
+++ src/components/nav.svelte
@@ -0,0 +1,42 @@
+<script>
+	import SkewButton from './SkewButton.svelte';
+</script>
+
+<header class="group">
+	<nav>
+		<h1><a href="#">Logo</a></h1>
+		<ul>
+			<!-- <li class="home selected"><a href="#"><span>Home</span></a></li>
+			<li class="about"><a href="#"><span>Projects</span></a></li>
+			<li class="work"><a href="#"><span>Tools</span></a></li>
+			<li class="contact"><a href="#"><span>About</span></a></li> -->
+			<div><SkewButton color="#261d1d"><a href="#"><span>Home</span></a></SkewButton></div>
+			<div><SkewButton color="#261d1d"><a href="#"><span>Home</span></a></SkewButton></div>
+
+			<div><SkewButton color="#261d1d"><a href="#"><span>Home</span></a></SkewButton></div>
+
+			<div><SkewButton color="#261d1d"><a href="#"><span>Home</span></a></SkewButton></div>
+
+			<div><SkewButton color="#261d1d"><a href="#"><span>Home</span></a></SkewButton></div>
+		</ul>
+	</nav>
+</header>
+
+<style>
+	header {
+		padding: 10px 50px;
+	}
+
+	nav {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 10px;
+	}
+
+	nav ul {
+		display: flex;
+		list-style: none;
+		gap: 1rem;
+	}
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Nav from '../../src/components/nav.svelte';
+	import SkewButton from '../../src/components/SkewButton.svelte';
+	import '../../src/styles/globals.css';
+</script>
+
+<div class="container">
+	<Nav />
+	<slot />
+	<SkewButton />
+</div>
+
+<style>
+	.container {
+		background-color: var(--primary-color-900);
+		color: white;
+		height: 100vh;
+	}
+</style>
