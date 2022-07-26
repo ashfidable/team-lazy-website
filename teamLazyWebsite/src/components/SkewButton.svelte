38c4d9b3924c04c215d50715a0dd908df16d66ed
RoastedBrotato <waleedajaz1996@gmail.com>
2022-07-27 02:04:53 +0300

    Co-authored-by: Ashfid <ashfidable@users.noreply.github.com>

---
+++ teamLazyWebsite/src/components/SkewButton.svelte
@@ -0,0 +1,32 @@
+<script>
+	export let color;
+</script>
+
+<a style:--bg-clr={color}><slot /></a>
+
+<style>
+	a {
+		--bg-clr: gray;
+		display: inline-block;
+		color: white;
+		position: relative;
+		border: none;
+		background-color: transparent;
+		padding: 0.9rem 2rem;
+		isolation: isolate;
+		/* z-index: 1; */
+	}
+
+	a::before {
+		content: '';
+		position: absolute;
+		inset: 0;
+		background-color: var(--bg-clr);
+		transform: skewX(-30deg);
+		z-index: -1;
+	}
+
+	a:hover::before {
+		border: 2px solid var(--accent-color-500);
+	}
+</style>
---
+++ teamLazyWebsite/src/routes/index.svelte
@@ -0,0 +1,10 @@
+<div>
+	<h1>Welcome to Persona</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
+
+<style>
+	div {
+		background-color: red;
+	}
+</style>
